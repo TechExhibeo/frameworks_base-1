33b0f546e1d9f8ca49f320c6f9e2952a9955398e
tiger_huang <tiger_huang@htc.com>
2014-10-23 20:24:19 +0800

    Prevent wrong system ui visibility callback after the user swipe
    If we return vis without clearing clearable flags, it will be set to
    IStatusBarService in updateSystemUiVisibilityLw(), and then be
    callbacked to WMS via statusBarVisibilityChanged(), and then be
    dispatched to the client; which is wrong because WMS should dispatch
    the new value to the client, not the old one.
    
    https://code.google.com/p/android/issues/detail?id=79991
    
    Change-Id: I2a06d1fbe66e854e639d86a39784f8c6a3303674
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -6437,6 +6437,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (denyTransientStatus || denyTransientNav) {
             // clear the clearable flags instead
             clearClearableFlagsLw();
+            vis &= ~View.SYSTEM_UI_CLEARABLE_FLAGS;
         }
 
         vis = mStatusBarController.updateVisibilityLw(transientStatusBarAllowed, oldVis, vis);
