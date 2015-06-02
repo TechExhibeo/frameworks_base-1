1e7b061b00abf2506447f441195bed8c91af6102
Jackeagle <jackeagle102@gmail.com>
2015-06-02 03:11:24 -0400

    Revert "Support GESTURE_SENSOR input device type with GestureService"
    This reverts commit e0458086b1bfb4e0b0d1977ecbb81f7609e338d6.

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -69,7 +69,6 @@ import com.android.server.display.DisplayManagerService;
 import com.android.server.dreams.DreamManagerService;
 import com.android.server.fingerprint.FingerprintService;
 import com.android.server.hdmi.HdmiControlService;
-import com.android.server.gesture.GestureService;
 import com.android.server.input.InputManagerService;
 import com.android.server.job.JobSchedulerService;
 import com.android.server.lights.LightsManager;
@@ -535,7 +534,6 @@ public final class SystemServer {
         LockSettingsService lockSettings = null;
         AssetAtlasService atlas = null;
         MediaRouterService mediaRouter = null;
-        GestureService gestureService = null;
 
         // Bring up services needed for UI.
         if (mFactoryTestMode != FactoryTest.FACTORY_TEST_LOW_LEVEL) {
@@ -933,17 +931,6 @@ public final class SystemServer {
                 }
             }
 
-            if (context.getResources().getBoolean(
-                    com.android.internal.R.bool.config_enableGestureService)) {
-                try {
-                    Slog.i(TAG, "Gesture Sensor Service");
-                    gestureService = new GestureService(context, inputManager);
-                    ServiceManager.addService("gesture", gestureService);
-                } catch (Throwable e) {
-                    Slog.e(TAG, "Failure starting Gesture Sensor Service", e);
-                }
-            }
-
             if (mPackageManager.hasSystemFeature(PackageManager.FEATURE_PRINTING)) {
                 mSystemServiceManager.startService(PRINT_MANAGER_SERVICE_CLASS);
             }
@@ -1101,14 +1088,6 @@ public final class SystemServer {
             mDisplayManagerService.systemReady(safeMode, mOnlyCore);
         } catch (Throwable e) {
             reportWtf("making Display Manager Service ready", e);
-        }
-
-        if (gestureService != null) {
-            try {
-                gestureService.systemReady();
-            } catch (Throwable e) {
-                reportWtf("making Gesture Sensor Service ready", e);
-            }
         }
 
         // These are needed to propagate to the runnable below.
