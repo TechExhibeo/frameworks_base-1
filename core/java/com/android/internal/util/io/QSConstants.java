SlimRoms/frameworks_base

[1/2] Custom QS
QuickSettings: Add long click support

Add long click support for QS tiles, and add actions for common tiles

Change-Id: I2b5940e1bf8ec80f03901fc2b017df68161b48ae

Add customization for Quick Settings [1/2]

Allow Quick Settings tiles to be arranged any way you want, delete them or add some.

I removed the "dual" status that was hardcoded into Wifi and BT tiles and added it
as a variable which is true when the tile is on the first row.

Also, the first row is now hardcoded to have only 2 items to follow the same design as before.

Inversion and Hotspot tile are no longer contextual and can be added, rmeoved or rearranged at will.

Settings: http://review.cyanogenmod.org/83418

Change-Id: Ie596a54a5c4eb777782fb047496cfcd0a6cd2e89

QS: change location sensors mode in quick settings [1/2]

Clicking on the location tile now opens a detail view
where we can either enable/disable location or
switch through the location modes.
Added a new icon for battery saving mode.
Added a switch in notification drawer settings
to enable this.

Change-Id: Icfc2e75aeafde32b1975040d0ea757aaeb34c120

Conflicts:
	packages/SystemUI/res/values/cm_strings.xml

QS: Don't show inversion and hotspot tiles by default

If you do a clean install, these tiles will be active. Disabled that and don't show them by default.

Change-Id: I4604a970e45825d77f056a65f4a3a6abc71e5d68

Quick settings: Option to disable brightness slider (1/2)

also fixed ripple placement for dual tiles

before:
https://www.dropbox.com/s/r4m1ytiwbdbj1xw/Screenshot_2015-01-12-21-29-55.png?dl=0

after:
https://www.dropbox.com/s/lvswhsubux548xh/Screenshot_2015-01-12-21-27-31.png?dl=0

Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>

Change-Id: I98a28e99e78ee4d8bb4f03bc53e867723d62b052

Migrate QS_TILES to Settings.Secure

Change-Id: I42783f574d7e050a04e960c91883f2aefc8ab7d6

Conflicts:
	core/java/android/provider/Settings.java
	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java

Syste

core/java/com/android/internal/util/slim/QSConstants.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.android.internal.util.slim;
+
+import java.util.ArrayList;
+
+public class QSConstants {
+    private QSConstants() {}
+
+    public static final String TILE_WIFI = "wifi";
+    public static final String TILE_BLUETOOTH = "bt";
+    public static final String TILE_INVERSION = "inversion";
+    public static final String TILE_CELLULAR = "cell";
+    public static final String TILE_AIRPLANE = "airplane";
+    public static final String TILE_ROTATION = "rotation";
+    public static final String TILE_FLASHLIGHT = "flashlight";
+    public static final String TILE_LOCATION = "location";
+    public static final String TILE_CAST = "cast";
+    public static final String TILE_HOTSPOT = "hotspot";
+    public static final String TILE_NOTIFICATIONS = "notifications";
+    public static final String TILE_DATA = "data";
+    public static final String TILE_ROAMING = "roaming";
+    public static final String TILE_DDS = "dds";
+    public static final String TILE_APN = "apn";
+    public static final String TILE_ADB_NETWORK = "adb_network";
+    public static final String TILE_NFC = "nfc";
+    public static final String TILE_COMPASS = "compass";
+    public static final String TILE_LOCKSCREEN = "lockscreen";
+    public static final String TILE_LTE = "lte";
+    public static final String TILE_VISUALIZER = "visualizer";
+    public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
+    public static final String TILE_USB_TETHER = "usb_tether";
+    public static final String TILE_REBOOT = "reboot";
+
+    // Order matters
+    protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
+
+    static {
+        TILES_DEFAULT.add(TILE_WIFI);
+        TILES_DEFAULT.add(TILE_BLUETOOTH);
+        TILES_DEFAULT.add(TILE_CELLULAR);
+        TILES_DEFAULT.add(TILE_AIRPLANE);
+        TILES_DEFAULT.add(TILE_ROTATION);
+        TILES_DEFAULT.add(TILE_FLASHLIGHT);
+        TILES_DEFAULT.add(TILE_LOCATION);
+        TILES_DEFAULT.add(TILE_CAST);
+    }
+
+    protected static final ArrayList<String> TILES_AVAILABLE = new ArrayList<String>();
+
+    static {
+        TILES_AVAILABLE.addAll(TILES_DEFAULT);
+        TILES_AVAILABLE.add(TILE_INVERSION);
+        TILES_AVAILABLE.add(TILE_HOTSPOT);
+        TILES_AVAILABLE.add(TILE_NOTIFICATIONS);
+        TILES_AVAILABLE.add(TILE_DATA);
+        TILES_AVAILABLE.add(TILE_ROAMING);
+        TILES_AVAILABLE.add(TILE_DDS);
+        TILES_AVAILABLE.add(TILE_APN);
+        TILES_AVAILABLE.add(TILE_ADB_NETWORK);
+        TILES_AVAILABLE.add(TILE_NFC);
+        TILES_AVAILABLE.add(TILE_COMPASS);
+        TILES_AVAILABLE.add(TILE_LOCKSCREEN);
+        TILES_AVAILABLE.add(TILE_LTE);
+        TILES_AVAILABLE.add(TILE_VISUALIZER);
+        TILES_AVAILABLE.add(TILE_SCREEN_TIMEOUT);
+        TILES_AVAILABLE.add(TILE_USB_TETHER);
+        TILES_AVAILABLE.add(TILE_REBOOT);
+    }
+}

core/java/com/android/internal/util/slim/QSUtils.java
@@ -1,0 +1,155 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.android.internal.util.slim;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.net.ConnectivityManager;
+import android.nfc.NfcAdapter;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import com.android.internal.telephony.PhoneConstants;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class QSUtils {
+    private static boolean sAvailableTilesFiltered;
+
+    private QSUtils() {}
+
+    public static List<String> getAvailableTiles(Context context) {
+        filterTiles(context);
+        return QSConstants.TILES_AVAILABLE;
+    }
+
+    public static List<String> getDefaultTiles(Context context) {
+        filterTiles(context);
+        return QSConstants.TILES_DEFAULT;
+    }
+
+    public static String getDefaultTilesAsString(Context context) {
+        List<String> list = getDefaultTiles(context);
+        return TextUtils.join(",", list);
+    }
+
+    private static void filterTiles(Context context) {
+        if (!sAvailableTilesFiltered) {
+            boolean deviceSupportsMobile = deviceSupportsMobileData(context);
+
+            // Tiles that need conditional filtering
+            Iterator<String> iterator = QSConstants.TILES_AVAILABLE.iterator();
+            while (iterator.hasNext()) {
+                String tileKey = iterator.next();
+                boolean removeTile = false;
+                switch (tileKey) {
+                    case QSConstants.TILE_CELLULAR:
+                    case QSConstants.TILE_HOTSPOT:
+                    case QSConstants.TILE_DATA:
+                    case QSConstants.TILE_ROAMING:
+                    case QSConstants.TILE_APN:
+                        removeTile = !deviceSupportsMobile;
+                        break;
+                    case QSConstants.TILE_DDS:
+                        removeTile = !deviceSupportsDdsSupported(context);
+                        break;
+                    case QSConstants.TILE_FLASHLIGHT:
+                        removeTile = !deviceSupportsFlashLight(context);
+                        break;
+                    case QSConstants.TILE_BLUETOOTH:
+                        removeTile = !deviceSupportsBluetooth();
+                        break;
+                    case QSConstants.TILE_NFC:
+                        removeTile = !deviceSupportsNfc(context);
+                        break;
+                    case QSConstants.TILE_COMPASS:
+                        removeTile = !deviceSupportsCompass(context);
+                        break;
+                }
+                if (removeTile) {
+                    iterator.remove();
+                    QSConstants.TILES_DEFAULT.remove(tileKey);
+                }
+            }
+
+            sAvailableTilesFiltered = true;
+        }
+    }
+
+    public static boolean deviceSupportsLte(Context ctx) {
+        final TelephonyManager tm = (TelephonyManager)
+                ctx.getSystemService(Context.TELEPHONY_SERVICE);
+        return (tm.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE)
+                || tm.getLteOnGsmMode() != 0;
+    }
+
+    public static boolean deviceSupportsDdsSupported(Context context) {
+        TelephonyManager tm = (TelephonyManager)
+                context.getSystemService(Context.TELEPHONY_SERVICE);
+        return tm.isMultiSimEnabled()
+                && tm.getMultiSimConfiguration() == TelephonyManager.MultiSimVariants.DSDA;
+    }
+
+    public static boolean deviceSupportsMobileData(Context ctx) {
+        ConnectivityManager cm = (ConnectivityManager) ctx.getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        return cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE);
+    }
+
+    public static boolean deviceSupportsBluetooth() {
+        return BluetoothAdapter.getDefaultAdapter() != null;
+    }
+
+    public static boolean deviceSupportsNfc(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        return packageManager.hasSystemFeature(PackageManager.FEATURE_NFC);
+    }
+
+    public static boolean deviceSupportsFlashLight(Context context) {
+        CameraManager cameraManager = (CameraManager) context.getSystemService(
+                Context.CAMERA_SERVICE);
+        try {
+            String[] ids = cameraManager.getCameraIdList();
+            for (String id : ids) {
+                CameraCharacteristics c = cameraManager.getCameraCharacteristics(id);
+                Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                if (flashAvailable != null
+                        && flashAvailable
+                        && lensFacing != null
+                        && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    return true;
+                }
+            }
+        } catch (CameraAccessException e) {
+            // Ignore
+        }
+        return false;
+    }
+
+    public static boolean deviceSupportsCompass(Context context) {
+        SensorManager sm = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        return sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null
+                && sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null;
+    }
+}

packages/SystemUI/src/com/android/systemui/qs/tiles/LteTile.java
@@ -1,0 +1,103 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+
+import com.android.internal.telephony.Phone;
+
+import com.android.internal.util.slim.QSUtils;
+import com.android.systemui.qs.QSTile;
+import com.android.systemui.R;
+
+/**
+ * Lazy Lte Tile
+ * Created by Adnan on 1/21/15.
+ */
+public class LteTile extends QSTile<QSTile.BooleanState> {
+    public LteTile(Host host) {
+        super(host);
+    }
+
+    @Override
+    protected BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleLongClick() {
+        super.handleLongClick();
+        mHost.startSettingsActivity(new Intent(Settings.ACTION_DATA_ROAMING_SETTINGS));
+    }
+
+    @Override
+    protected void handleClick() {
+        toggleLteState();
+        refreshState();
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        // Hide the tile if device doesn't support LTE
+        // or it supports Dual Sim Dual Active.
+        // TODO: Should be spawning off a tile per sim
+        if (!QSUtils.deviceSupportsLte(mContext)
+                || QSUtils.deviceSupportsDdsSupported(mContext)) {
+            state.visible = false;
+            return;
+        }
+
+        state.label = mContext.getString(R.string.quick_settings_lte_label);
+
+        switch (getCurrentPreferredNetworkMode()) {
+            case Phone.NT_MODE_GLOBAL:
+            case Phone.NT_MODE_LTE_CDMA_AND_EVDO:
+            case Phone.NT_MODE_LTE_GSM_WCDMA:
+            case Phone.NT_MODE_LTE_ONLY:
+            case Phone.NT_MODE_LTE_WCDMA:
+            case Phone.NT_MODE_LTE_CDMA_EVDO_GSM_WCDMA:
+            case Phone.NT_MODE_TD_SCDMA_GSM_WCDMA_LTE:
+            case Phone.NT_MODE_TD_SCDMA_WCDMA_LTE:
+                state.visible = true;
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_lte_on);
+                break;
+            default:
+                state.visible = true;
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_lte_off);
+                break;
+        }
+    }
+
+    private void toggleLteState() {
+        TelephonyManager tm = (TelephonyManager)
+                mContext.getSystemService(Context.TELEPHONY_SERVICE);
+        tm.toggleLTE(true);
+    }
+
+    private int getCurrentPreferredNetworkMode() {
+        return Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.PREFERRED_NETWORK_MODE, -1);
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+
+    }
+}

TechExhibeo/frameworks_base-1

core/java/com/android/internal/util/io/QSConstants.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.android.internal.util.io;
+
+import java.util.ArrayList;
+
+public class QSConstants {
+    private QSConstants() {}
+
+    public static final String TILE_WIFI = "wifi";
+    public static final String TILE_BLUETOOTH = "bt";
+    public static final String TILE_INVERSION = "inversion";
+    public static final String TILE_CELLULAR = "cell";
+    public static final String TILE_AIRPLANE = "airplane";
+    public static final String TILE_ROTATION = "rotation";
+    public static final String TILE_FLASHLIGHT = "flashlight";
+    public static final String TILE_LOCATION = "location";
+    public static final String TILE_CAST = "cast";
+    public static final String TILE_HOTSPOT = "hotspot";
+    public static final String TILE_NOTIFICATIONS = "notifications";
+    public static final String TILE_DATA = "data";
+    public static final String TILE_ROAMING = "roaming";
+    public static final String TILE_DDS = "dds";
+    public static final String TILE_APN = "apn";
+    public static final String TILE_ADB_NETWORK = "adb_network";
+    public static final String TILE_NFC = "nfc";
+    public static final String TILE_COMPASS = "compass";
+    public static final String TILE_LOCKSCREEN = "lockscreen";
+    public static final String TILE_LTE = "lte";
+    public static final String TILE_VISUALIZER = "visualizer";
+    public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
+    public static final String TILE_USB_TETHER = "usb_tether";
+    public static final String TILE_REBOOT = "reboot";
+
+    // Order matters
+    protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
+
+    static {
+        TILES_DEFAULT.add(TILE_WIFI);
+        TILES_DEFAULT.add(TILE_BLUETOOTH);
+        TILES_DEFAULT.add(TILE_CELLULAR);
+        TILES_DEFAULT.add(TILE_AIRPLANE);
+        TILES_DEFAULT.add(TILE_ROTATION);
+        TILES_DEFAULT.add(TILE_FLASHLIGHT);
+        TILES_DEFAULT.add(TILE_LOCATION);
+        TILES_DEFAULT.add(TILE_CAST);
+    }
+
+    protected static final ArrayList<String> TILES_AVAILABLE = new ArrayList<String>();
+
+    static {
+        TILES_AVAILABLE.addAll(TILES_DEFAULT);
+        TILES_AVAILABLE.add(TILE_INVERSION);
+        TILES_AVAILABLE.add(TILE_HOTSPOT);
+        TILES_AVAILABLE.add(TILE_NOTIFICATIONS);
+        TILES_AVAILABLE.add(TILE_DATA);
+        TILES_AVAILABLE.add(TILE_ROAMING);
+        TILES_AVAILABLE.add(TILE_DDS);
+        TILES_AVAILABLE.add(TILE_APN);
+        TILES_AVAILABLE.add(TILE_ADB_NETWORK);
+        TILES_AVAILABLE.add(TILE_NFC);
+        TILES_AVAILABLE.add(TILE_COMPASS);
+        TILES_AVAILABLE.add(TILE_LOCKSCREEN);
+        TILES_AVAILABLE.add(TILE_LTE);
+        TILES_AVAILABLE.add(TILE_VISUALIZER);
+        TILES_AVAILABLE.add(TILE_SCREEN_TIMEOUT);
+        TILES_AVAILABLE.add(TILE_USB_TETHER);
+        TILES_AVAILABLE.add(TILE_REBOOT);
+    }
+}

core/java/com/android/internal/util/io/QSUtils.java
@@ -1,0 +1,155 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package com.android.internal.util.io;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.net.ConnectivityManager;
+import android.nfc.NfcAdapter;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import com.android.internal.telephony.PhoneConstants;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class QSUtils {
+    private static boolean sAvailableTilesFiltered;
+
+    private QSUtils() {}
+
+    public static List<String> getAvailableTiles(Context context) {
+        filterTiles(context);
+        return QSConstants.TILES_AVAILABLE;
+    }
+
+    public static List<String> getDefaultTiles(Context context) {
+        filterTiles(context);
+        return QSConstants.TILES_DEFAULT;
+    }
+
+    public static String getDefaultTilesAsString(Context context) {
+        List<String> list = getDefaultTiles(context);
+        return TextUtils.join(",", list);
+    }
+
+    private static void filterTiles(Context context) {
+        if (!sAvailableTilesFiltered) {
+            boolean deviceSupportsMobile = deviceSupportsMobileData(context);
+
+            // Tiles that need conditional filtering
+            Iterator<String> iterator = QSConstants.TILES_AVAILABLE.iterator();
+            while (iterator.hasNext()) {
+                String tileKey = iterator.next();
+                boolean removeTile = false;
+                switch (tileKey) {
+                    case QSConstants.TILE_CELLULAR:
+                    case QSConstants.TILE_HOTSPOT:
+                    case QSConstants.TILE_DATA:
+                    case QSConstants.TILE_ROAMING:
+                    case QSConstants.TILE_APN:
+                        removeTile = !deviceSupportsMobile;
+                        break;
+                    case QSConstants.TILE_DDS:
+                        removeTile = !deviceSupportsDdsSupported(context);
+                        break;
+                    case QSConstants.TILE_FLASHLIGHT:
+                        removeTile = !deviceSupportsFlashLight(context);
+                        break;
+                    case QSConstants.TILE_BLUETOOTH:
+                        removeTile = !deviceSupportsBluetooth();
+                        break;
+                    case QSConstants.TILE_NFC:
+                        removeTile = !deviceSupportsNfc(context);
+                        break;
+                    case QSConstants.TILE_COMPASS:
+                        removeTile = !deviceSupportsCompass(context);
+                        break;
+                }
+                if (removeTile) {
+                    iterator.remove();
+                    QSConstants.TILES_DEFAULT.remove(tileKey);
+                }
+            }
+
+            sAvailableTilesFiltered = true;
+        }
+    }
+
+    public static boolean deviceSupportsLte(Context ctx) {
+        final TelephonyManager tm = (TelephonyManager)
+                ctx.getSystemService(Context.TELEPHONY_SERVICE);
+        return (tm.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE)
+                || tm.getLteOnGsmMode() != 0;
+    }
+
+    public static boolean deviceSupportsDdsSupported(Context context) {
+        TelephonyManager tm = (TelephonyManager)
+                context.getSystemService(Context.TELEPHONY_SERVICE);
+        return tm.isMultiSimEnabled()
+                && tm.getMultiSimConfiguration() == TelephonyManager.MultiSimVariants.DSDA;
+    }
+
+    public static boolean deviceSupportsMobileData(Context ctx) {
+        ConnectivityManager cm = (ConnectivityManager) ctx.getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        return cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE);
+    }
+
+    public static boolean deviceSupportsBluetooth() {
+        return BluetoothAdapter.getDefaultAdapter() != null;
+    }
+
+    public static boolean deviceSupportsNfc(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        return packageManager.hasSystemFeature(PackageManager.FEATURE_NFC);
+    }
+
+    public static boolean deviceSupportsFlashLight(Context context) {
+        CameraManager cameraManager = (CameraManager) context.getSystemService(
+                Context.CAMERA_SERVICE);
+        try {
+            String[] ids = cameraManager.getCameraIdList();
+            for (String id : ids) {
+                CameraCharacteristics c = cameraManager.getCameraCharacteristics(id);
+                Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                if (flashAvailable != null
+                        && flashAvailable
+                        && lensFacing != null
+                        && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    return true;
+                }
+            }
+        } catch (CameraAccessException e) {
+            // Ignore
+        }
+        return false;
+    }
+
+    public static boolean deviceSupportsCompass(Context context) {
+        SensorManager sm = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        return sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null
+                && sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null;
+    }
+}

packages/SystemUI/src/com/android/systemui/qs/tiles/LteTile.java
@@ -1,0 +1,103 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+
+import com.android.internal.telephony.Phone;
+
+import com.android.internal.util.io.QSUtils;
+import com.android.systemui.qs.QSTile;
+import com.android.systemui.R;
+
+/**
+ * Lazy Lte Tile
+ * Created by Adnan on 1/21/15.
+ */
+public class LteTile extends QSTile<QSTile.BooleanState> {
+    public LteTile(Host host) {
+        super(host);
+    }
+
+    @Override
+    protected BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleLongClick() {
+        super.handleLongClick();
+        mHost.startSettingsActivity(new Intent(Settings.ACTION_DATA_ROAMING_SETTINGS));
+    }
+
+    @Override
+    protected void handleClick() {
+        toggleLteState();
+        refreshState();
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        // Hide the tile if device doesn't support LTE
+        // or it supports Dual Sim Dual Active.
+        // TODO: Should be spawning off a tile per sim
+        if (!QSUtils.deviceSupportsLte(mContext)
+                || QSUtils.deviceSupportsDdsSupported(mContext)) {
+            state.visible = false;
+            return;
+        }
+
+        state.label = mContext.getString(R.string.quick_settings_lte_label);
+
+        switch (getCurrentPreferredNetworkMode()) {
+            case Phone.NT_MODE_GLOBAL:
+            case Phone.NT_MODE_LTE_CDMA_AND_EVDO:
+            case Phone.NT_MODE_LTE_GSM_WCDMA:
+            case Phone.NT_MODE_LTE_ONLY:
+            case Phone.NT_MODE_LTE_WCDMA:
+            case Phone.NT_MODE_LTE_CDMA_EVDO_GSM_WCDMA:
+            case Phone.NT_MODE_TD_SCDMA_GSM_WCDMA_LTE:
+            case Phone.NT_MODE_TD_SCDMA_WCDMA_LTE:
+                state.visible = true;
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_lte_on);
+                break;
+            default:
+                state.visible = true;
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_lte_off);
+                break;
+        }
+    }
+
+    private void toggleLteState() {
+        TelephonyManager tm = (TelephonyManager)
+                mContext.getSystemService(Context.TELEPHONY_SERVICE);
+        tm.toggleLTE(true);
+    }
+
+    private int getCurrentPreferredNetworkMode() {
+        return Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.PREFERRED_NETWORK_MODE, -1);
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+
+    }
+}